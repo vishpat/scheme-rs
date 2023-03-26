8018d637e97307d963498fec3ed1707699b612eb
Vishal Patil <vishpat@gmail.com>
2023-03-26 17:39:42 -0400

    Parameterized the env

--- src/env.rs
+++ src/env.rs
@@ -1,17 +1,21 @@
-use crate::object::Object;
 use std::cell::RefCell;
 use std::collections::HashMap;
 use std::rc::Rc;
 
 #[derive(Debug, PartialEq, Default)]
-pub struct Env {
-    parent: Option<Rc<RefCell<Env>>>,
-    pub vars: HashMap<String, Object>,
+pub struct Env<T> {
+    parent: Option<Rc<RefCell<Env<T>>>>,
+    pub vars: HashMap<String, T>,
 }
 
-impl Env {
+impl <T:Clone>Env<T> {
     pub fn new() -> Self {
-        Default::default()
+        {
+            Env {
+                vars: HashMap::new(),
+                parent: None,
+            }
+        }
     }
 
     pub fn update(&mut self, data: Rc<RefCell<Self>>) {
@@ -23,21 +27,21 @@ impl Env {
         );
     }
 
-    pub fn extend(parent: Rc<RefCell<Self>>) -> Env {
+    pub fn extend(parent: Rc<RefCell<Self>>) -> Env<T> {
         Env {
             vars: HashMap::new(),
             parent: Some(parent),
         }
     }
 
-    pub fn get(&self, name: &str) -> Option<Object> {
+    pub fn get(&self, name: &str) -> Option<T> {
         match self.vars.get(name) {
             Some(value) => Some(value.clone()),
             None => self.parent.as_ref().and_then(|o| o.borrow().get(name)),
         }
     }
 
-    pub fn set(&mut self, name: &str, val: Object) {
+    pub fn set(&mut self, name: &str, val: T) {
         self.vars.insert(name.to_string(), val);
     }
 }
