705c423c07b8d936482367374715d47e8d3c22c4
Vishal Patil <vishpat@gmail.com>
2023-03-28 20:06:13 -0400

    Re-factor the code

--- src/compiler.rs
+++ src/compiler.rs
@@ -72,6 +72,146 @@ fn process_symbol<'ctx>(compiler: &'ctx Compiler, sym: &str) -> Result<AnyValueE
     Ok(x.as_any_value_enum())
 }
 
+fn compile_define<'a>(
+    compiler: &'a Compiler,
+    list: &'a Vec<Object>,
+) -> Result<FloatValue<'a>, String> {
+    if list.len() != 3 {
+        return Err(format!("Expected 2 arguments, found {}", list.len() - 1));
+    }
+    let name = match &list[1] {
+        Object::Symbol(s) => s,
+        _ => return Err("Expected symbol".to_string()),
+    };
+    let val = compile_obj(compiler, &list[2])?;
+    let ptr = compiler
+        .builder
+        .build_alloca(compiler.context.f64_type(), name);
+    compiler.builder.build_store(ptr, val);
+    compiler.env.borrow_mut().set(
+        name,
+        Pointer {
+            ptr,
+            data_type: DataType::Number,
+        },
+    );
+    Ok(compiler.context.f64_type().const_zero())
+}
+
+fn compile_if<'a>(compiler: &'a Compiler, list: &'a Vec<Object>) -> Result<FloatValue<'a>, String> {
+    if list.len() != 4 {
+        return Err(format!("Expected 3 arguments, found {}", list.len() - 1));
+    }
+    let cond_ir = compile_obj(compiler, &list[1])?;
+    let cond_bool = compiler.builder.build_float_compare(
+        inkwell::FloatPredicate::ONE,
+        cond_ir,
+        compiler.context.f64_type().const_zero(),
+        "ifcond",
+    );
+
+    let curr_func = compiler
+        .builder
+        .get_insert_block()
+        .unwrap()
+        .get_parent()
+        .expect("no parent");
+
+    let mut then_bb = compiler.context.append_basic_block(curr_func, "then");
+    let mut else_bb = compiler.context.append_basic_block(curr_func, "else");
+    let merge_bb = compiler
+        .context
+        .append_basic_block(curr_func, "if_continue");
+
+    compiler
+        .builder
+        .build_conditional_branch(cond_bool, then_bb, else_bb);
+
+    compiler.builder.position_at_end(then_bb);
+    let then_val = compile_obj(compiler, &list[2])?;
+    compiler.builder.build_unconditional_branch(merge_bb);
+    then_bb = compiler.builder.get_insert_block().unwrap();
+
+    compiler.builder.position_at_end(else_bb);
+    let else_val = compile_obj(compiler, &list[3])?;
+    compiler.builder.build_unconditional_branch(merge_bb);
+    else_bb = compiler.builder.get_insert_block().unwrap();
+
+    compiler.builder.position_at_end(merge_bb);
+    let phi = compiler
+        .builder
+        .build_phi(compiler.context.f64_type(), "iftmp");
+    phi.add_incoming(&[(&then_val, then_bb), (&else_val, else_bb)]);
+
+    Ok(phi.as_basic_value().into_float_value())
+}
+
+fn compile_binary_expr<'a>(
+    binary_op: &str,
+    compiler: &'a Compiler,
+    list: &'a Vec<Object>,
+) -> Result<FloatValue<'a>, String> {
+    if list.len() != 3 {
+        return Err(format!("Expected 2 arguments, found {}", list.len() - 1));
+    }
+
+    let left = match &list[1] {
+        Object::Number(n) => compile_number(compiler, n)?,
+        Object::Symbol(s) => {
+            let val = process_symbol(compiler, s)?;
+            match val {
+                AnyValueEnum::FloatValue(v) => v,
+                _ => return Err(format!("Cannot compile lhs: {:?}", list[1])),
+            }
+        }
+        Object::List(l) => compile_list(compiler, l)?,
+        _ => return Err(format!("Cannot compile lhs: {:?}", list[1])),
+    };
+
+    let right = match &list[2] {
+        Object::Number(n) => compile_number(compiler, n)?,
+        Object::Symbol(s) => {
+            let val = process_symbol(compiler, s)?;
+            match val {
+                AnyValueEnum::FloatValue(v) => v,
+                _ => return Err(format!("Cannot compile lhs: {:?}", list[1])),
+            }
+        }
+        Object::List(l) => compile_list(compiler, l)?,
+        _ => return Err(format!("Cannot compile rhs: {:?}", list[2])),
+    };
+
+    let val = match binary_op {
+        "+" => compiler.builder.build_float_add(left, right, "addtmp"),
+        "-" => compiler.builder.build_float_sub(left, right, "subtmp"),
+        "*" => compiler.builder.build_float_mul(left, right, "multmp"),
+        "/" => compiler.builder.build_float_div(left, right, "divtmp"),
+        ">" | "<" | ">=" | "<=" | "==" | "!=" => {
+            let op = match binary_op {
+                ">" => FloatPredicate::UGT,
+                "<" => FloatPredicate::ULT,
+                ">=" => FloatPredicate::UGE,
+                "<=" => FloatPredicate::ULE,
+                "==" => FloatPredicate::UEQ,
+                "!=" => FloatPredicate::UNE,
+                _ => return Err(format!("Cannot compile list 1.: {:?}", list)),
+            };
+
+            let cmp_as_intval = compiler
+                .builder
+                .build_float_compare(op, left, right, "cmptmp");
+
+            compiler.builder.build_unsigned_int_to_float(
+                cmp_as_intval,
+                compiler.context.f64_type(),
+                "booltmp",
+            )
+        }
+        _ => return Err(format!("Cannot compile list 2.: {:?}", list)),
+    };
+    Ok(val)
+}
+
 fn compile_list<'a>(
     compiler: &'a Compiler,
     list: &'a Vec<Object>,
@@ -84,135 +224,10 @@ fn compile_list<'a>(
 
     match &list[0] {
         Object::Symbol(s) => match s.as_str() {
-            "define" => {
-                if list.len() != 3 {
-                    return Err(format!("Expected 2 arguments, found {}", list.len() - 1));
-                }
-                let name = match &list[1] {
-                    Object::Symbol(s) => s,
-                    _ => return Err("Expected symbol".to_string()),
-                };
-                let val = compile_obj(compiler, &list[2])?;
-                let ptr = compiler
-                    .builder
-                    .build_alloca(compiler.context.f64_type(), name);
-                compiler.builder.build_store(ptr, val);
-                compiler.env.borrow_mut().set(
-                    name,
-                    Pointer {
-                        ptr,
-                        data_type: DataType::Number,
-                    },
-                );
-                Ok(compiler.context.f64_type().const_zero())
-            }
-            "if" => {
-                if list.len() != 4 {
-                    return Err(format!("Expected 3 arguments, found {}", list.len() - 1));
-                }
-                let cond_ir = compile_obj(compiler, &list[1])?;
-                let cond_bool = compiler.builder.build_float_compare(
-                    inkwell::FloatPredicate::ONE,
-                    cond_ir,
-                    compiler.context.f64_type().const_zero(),
-                    "ifcond",
-                );
-
-                let curr_func = compiler
-                    .builder
-                    .get_insert_block()
-                    .unwrap()
-                    .get_parent()
-                    .expect("no parent");
-
-                let mut then_bb = compiler.context.append_basic_block(curr_func, "then");
-                let mut else_bb = compiler.context.append_basic_block(curr_func, "else");
-                let merge_bb = compiler
-                    .context
-                    .append_basic_block(curr_func, "if_continue");
-
-                compiler
-                    .builder
-                    .build_conditional_branch(cond_bool, then_bb, else_bb);
-
-                compiler.builder.position_at_end(then_bb);
-                let then_val = compile_obj(compiler, &list[2])?;
-                compiler.builder.build_unconditional_branch(merge_bb);
-                then_bb = compiler.builder.get_insert_block().unwrap();
-
-                compiler.builder.position_at_end(else_bb);
-                let else_val = compile_obj(compiler, &list[3])?;
-                compiler.builder.build_unconditional_branch(merge_bb);
-                else_bb = compiler.builder.get_insert_block().unwrap();
-
-                compiler.builder.position_at_end(merge_bb);
-                let phi = compiler
-                    .builder
-                    .build_phi(compiler.context.f64_type(), "iftmp");
-                phi.add_incoming(&[(&then_val, then_bb), (&else_val, else_bb)]);
-
-                Ok(phi.as_basic_value().into_float_value())
-            }
+            "define" => compile_define(compiler, list),
+            "if" => compile_if(compiler, list),
             "+" | "-" | "*" | "/" | ">" | "<" | ">=" | "<=" | "==" | "!=" => {
-                if list.len() != 3 {
-                    return Err(format!("Expected 2 arguments, found {}", list.len() - 1));
-                }
-
-                let left = match &list[1] {
-                    Object::Number(n) => compile_number(compiler, n)?,
-                    Object::Symbol(s) => {
-                        let val = process_symbol(compiler, s)?;
-                        match val {
-                            AnyValueEnum::FloatValue(v) => v,
-                            _ => return Err(format!("Cannot compile lhs: {:?}", list[1])),
-                        }
-                    }
-                    Object::List(l) => compile_list(compiler, l)?,
-                    _ => return Err(format!("Cannot compile lhs: {:?}", list[1])),
-                };
-
-                let right = match &list[2] {
-                    Object::Number(n) => compile_number(compiler, n)?,
-                    Object::Symbol(s) => {
-                        let val = process_symbol(compiler, s)?;
-                        match val {
-                            AnyValueEnum::FloatValue(v) => v,
-                            _ => return Err(format!("Cannot compile lhs: {:?}", list[1])),
-                        }
-                    }
-                    Object::List(l) => compile_list(compiler, l)?,
-                    _ => return Err(format!("Cannot compile rhs: {:?}", list[2])),
-                };
-
-                let val = match s.as_str() {
-                    "+" => compiler.builder.build_float_add(left, right, "addtmp"),
-                    "-" => compiler.builder.build_float_sub(left, right, "subtmp"),
-                    "*" => compiler.builder.build_float_mul(left, right, "multmp"),
-                    "/" => compiler.builder.build_float_div(left, right, "divtmp"),
-                    ">" | "<" | ">=" | "<=" | "==" | "!=" => {
-                        let op = match s.as_str() {
-                            ">" => FloatPredicate::UGT,
-                            "<" => FloatPredicate::ULT,
-                            ">=" => FloatPredicate::UGE,
-                            "<=" => FloatPredicate::ULE,
-                            "==" => FloatPredicate::UEQ,
-                            "!=" => FloatPredicate::UNE,
-                            _ => return Err(format!("Cannot compile list 1.: {:?}", list)),
-                        };
-
-                        let cmp_as_intval = compiler
-                            .builder
-                            .build_float_compare(op, left, right, "cmptmp");
-
-                        compiler.builder.build_unsigned_int_to_float(
-                            cmp_as_intval,
-                            compiler.context.f64_type(),
-                            "booltmp",
-                        )
-                    }
-                    _ => return Err(format!("Cannot compile list 2.: {:?}", list)),
-                };
-                Ok(val)
+                compile_binary_expr(s, compiler, list)
             }
             _ => return Err(format!("Cannot compile list 3.: {:?}", list)),
         },
@@ -249,6 +264,7 @@ pub fn compile_program(program: &str) -> Result<(), String> {
         Object::List(list) => {
             for obj in list {
                 let val = compile_obj(&compiler, &obj)?;
+                debug!("Got float value: {:?}", val);
                 main_val = val.as_any_value_enum().into_float_value().get_constant();
             }
         }
@@ -256,7 +272,9 @@ pub fn compile_program(program: &str) -> Result<(), String> {
     }
     let ret_val = main_val
         .map(|v| BasicValueEnum::FloatValue(compiler.context.f64_type().const_float(v.0)))
-        .unwrap();
+        .unwrap_or_else( || BasicValueEnum::FloatValue(
+            compiler.context.f64_type().const_float(0.0),
+        ));
 
     compiler.builder.build_return(Some(&ret_val));
     compiler.fpm.run_on(&main_func);
