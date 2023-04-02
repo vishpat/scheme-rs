a2e5c79f7e5a2315a08e3194fe8bd3844ee29de7
Vishal Patil <vishpat@gmail.com>
2023-04-01 22:48:01 -0400

    Able to access global variables

--- src/compiler.rs
+++ src/compiler.rs
@@ -10,6 +10,7 @@ use inkwell::values::AnyValueEnum;
 use inkwell::values::BasicMetadataValueEnum;
 use inkwell::values::PointerValue;
 use inkwell::values::{FloatValue, FunctionValue};
+use inkwell::AddressSpace;
 use inkwell::FloatPredicate;
 use log::debug;
 use std::cell::RefCell;
@@ -59,6 +60,7 @@ impl<'ctx> SymTables<'ctx> {
             debug!("Got symbol {} val: {:?} from sym table", name, val);
             return Some(val.clone());
         }
+
         None
     }
 }
@@ -93,7 +95,18 @@ fn compile_number<'a>(compiler: &'a Compiler, n: &'a f64) -> Result<FloatValue<'
 }
 
 fn process_symbol<'ctx>(compiler: &'ctx Compiler, sym: &str) -> Result<AnyValueEnum<'ctx>, String> {
+    let global_val = compiler.module.get_global(sym);
+    if let Some(g) = global_val {
+        let val = g.get_initializer().unwrap();
+        debug!("Loading global symbol: {} with value {:?}", sym, val);
+        compiler
+            .builder
+            .build_load(compiler.context.f64_type(), g.as_pointer_value(), sym);
+        return Ok(val.as_any_value_enum());
+    }
+
     let val = compiler.sym_tables.borrow().get_symbol_value(sym);
+
     debug!("Processing symbol {} val: {:?}", sym, val);
     let x = match val {
         Some(p) => {
@@ -254,13 +267,12 @@ fn compile_define_obj<'a>(
         .build_alloca(compiler.context.f64_type(), name);
     compiler.builder.build_store(ptr, val);
 
-    compiler.sym_tables.borrow_mut().add_symbol_value(
+    let global_val = compiler.module.add_global(
+        compiler.context.f64_type(),
+        Some(AddressSpace::default()),
         name,
-        Pointer {
-            ptr,
-            data_type: DataType::Number,
-        },
     );
+    global_val.set_initializer(&val);
 
     Ok(compiler.context.f64_type().const_zero())
 }
@@ -427,9 +439,7 @@ fn compile_list<'a>(
             "+" | "-" | "*" | "/" | ">" | "<" | ">=" | "<=" | "==" | "!=" => {
                 compile_binary_expr(s, compiler, list)
             }
-            _ => { 
-                compile_function_call(compiler, list)
-            },
+            _ => compile_function_call(compiler, list),
         },
         _ => return Err(format!("Cannot compile list 4.: {:?}", list)),
     }
